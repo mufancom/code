0a82cbc8a660adad0e88ec56b97b3df3e4a0b0b2
vilicvane <vilicvane@live.com>
2023-10-22 18:29:11 +0800

    Disable no-this-alias

--- packages/eslint-plugin/src/library/configs/@typescript.ts
+++ packages/eslint-plugin/src/library/configs/@typescript.ts
@@ -33,6 +33,7 @@ export default {
     ],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
     '@typescript-eslint/no-unsafe-declaration-merging': 'off',
     '@typescript-eslint/no-useless-constructor': 'error',
     '@typescript-eslint/no-unused-vars': ['error', NO_UNUSED_VARS_OPTIONS],
